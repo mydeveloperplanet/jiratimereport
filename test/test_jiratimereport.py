7ede6880ef8af6f0770fa2c6a5881b125e1b1fc0
mydeveloperplanet <gunter@mydeveloperplanet.com>
2020-02-16 10:46:06 +0100

    Moved the inlinde code json responses to json files.

--- test/test_jiratimereport.py
+++ test/test_jiratimereport.py
@@ -10,11 +10,9 @@ class MyTestCase(unittest.TestCase):
         """
         Test the single page response when retrieving Jira issues
         """
-        mock_response = '{"expand":"schema,names","startAt":0,"maxResults":50,"total":2,"issues":[{' \
-                        '"expand":"operations,versionedRepresentations,editmeta,changelog,renderedFields",' \
-                        '"id":"10005","self":"https://jira_url/rest/api/2/issue/10005", "key":"MYB-5"},' \
-                        '{"expand":"operations,versionedRepresentations,editmeta,changelog,renderedFields",' \
-                        '"id":"10004","self":"https://jira_url/rest/api/2/issue/10004","key":"MYB-4"}]}'
+        with open("issues_one_page.json", "r") as issues_file:
+            mock_response = issues_file.read()
+
         expected_result = [
             {'expand': 'operations,versionedRepresentations,editmeta,changelog,renderedFields', 'id': '10005',
              'self': 'https://jira_url/rest/api/2/issue/10005', 'key': 'MYB-5'},
@@ -31,16 +29,12 @@ class MyTestCase(unittest.TestCase):
         """
         Test the multiple pages response when retrieving Jira issues (pagination)
         """
-        mock_response_first_page = \
-            '{"expand":"schema,names","startAt":0,"maxResults":2,"total":3,"issues":[{' \
-            '"expand":"operations,versionedRepresentations,editmeta,changelog,renderedFields",' \
-            '"id":"10005","self":"https://jira_url/rest/api/2/issue/10005", "key":"MYB-5"},' \
-            '{"expand":"operations,versionedRepresentations,editmeta,changelog,renderedFields",' \
-            '"id":"10004","self":"https://jira_url/rest/api/2/issue/10004","key":"MYB-4"}]} '
-        mock_response_second_page = \
-            '{"expand":"schema,names","startAt":2,"maxResults":2,"total":3,"issues":[{' \
-            '"expand":"operations,versionedRepresentations,editmeta,changelog,renderedFields",' \
-            '"id":"10006","self":"https://jira_url/rest/api/2/issue/10006", "key":"MYB-6"}]} '
+        with open("issues_multiple_first_page.json", "r") as issues_first_file:
+            mock_response_first_page = issues_first_file.read()
+
+        with open("issues_multiple_second_page.json", "r") as issues_second_file:
+            mock_response_second_page = issues_second_file.read()
+
         expected_result = [
             {'expand': 'operations,versionedRepresentations,editmeta,changelog,renderedFields', 'id': '10005',
              'self': 'https://jira_url/rest/api/2/issue/10005', 'key': 'MYB-5'},
@@ -61,11 +55,10 @@ class MyTestCase(unittest.TestCase):
         """
         Test the single page response when retrieving Jira work logs
         """
-
-        with open("mock_response_first_issue.json", "r") as first_issue_file:
+        with open("work_logs_first_issue_one_page.json", "r") as first_issue_file:
             mock_response_first_issue = first_issue_file.read()
 
-        with open("mock_response_second_issue.json", "r") as second_issue_file:
+        with open("work_logs_second_issue_one_page.json", "r") as second_issue_file:
             mock_response_second_issue = second_issue_file.read()
 
         issues_json = [
