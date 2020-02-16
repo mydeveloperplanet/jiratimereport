9f530ec1e1d2aefbe429ea0322778fd5f49b1779
mydeveloperplanet <gunter@mydeveloperplanet.com>
2020-02-16 10:57:55 +0100

    Added unit test for multiple pages retrieval for work logs (pagination)

--- test/test_jiratimereport.py
+++ test/test_jiratimereport.py
@@ -78,5 +78,30 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(work_logs[2], WorkLog("MYB-4", datetime(2020, 1, 12), 3600, "John Doe"))
 
 
+    def test_get_work_logs_multiple_pages(self):
+        """
+        Test the multiple pages response when retrieving Jira issues (pagination)
+        """
+        with open("work_logs_multiple_first_page.json", "r") as issues_first_file:
+            mock_response_first_page = issues_first_file.read()
+
+        with open("work_logs_multiple_second_page.json", "r") as issues_second_file:
+            mock_response_second_page = issues_second_file.read()
+
+        issues_json = [
+            {'expand': 'operations,versionedRepresentations,editmeta,changelog,renderedFields', 'id': '10005',
+             'self': 'https://jira_url/rest/api/2/issue/10005', 'key': 'MYB-5'}]
+
+        with requests_mock.Mocker() as m:
+            m.register_uri('GET', '/rest/api/2/issue/MYB-5/worklog/', [{'text': mock_response_first_page},
+                                                                       {'text': mock_response_second_page}])
+            work_logs = jiratimereport.get_work_logs("https://jira_url", "user_name", "api_token",
+                                                     "2020-01-10", "2020-01-20", "", issues_json)
+
+        self.assertEqual(work_logs[0], WorkLog("MYB-5", datetime(2020, 1, 18), 3600, "John Doe"))
+        self.assertEqual(work_logs[1], WorkLog("MYB-5", datetime(2020, 1, 18), 5400, "John Doe"))
+        self.assertEqual(work_logs[2], WorkLog("MYB-5", datetime(2020, 1, 12), 3600, "John Doe"))
+
+
 if __name__ == '__main__':
     unittest.main()
